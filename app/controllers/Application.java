f55693d411ad466f15d3342526e23f09a9ff330f
Reimerei <reimer@posteo.de>
2013-04-25 23:14:17 +0200

    proper connection

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,15 +1,18 @@
 package controllers;
 
 import play.*;
+import play.db.DB;
 import play.mvc.*;
 
 import views.html.*;
+
+import java.sql.Connection;
 
 public class Application extends Controller {
 
     public static Result index() {
 
-
+        Connection c = DB.getConnection("mixbuilder");
 
         return ok(index.render("D"));
     }
