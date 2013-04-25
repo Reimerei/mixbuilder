ab30e7e032613e13d2e73b06db511bb65a8b7b43
Reimerei <reimerei@github.com>
2013-04-26 00:26:58 +0200

    added showcolumn

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,13 +7,32 @@ import play.mvc.*;
 import views.html.*;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class Application extends Controller {
 
-    public static Result index() {
-
-        Connection c = DB.getConnection("mixbuilder");
+    public static Result index() throws SQLException {
 
         return ok( index.render("D"));
+    }
+
+    public static Result showColumn(String columnNr) throws SQLException
+    {
+        Connection con = DB.getConnection("mixbuilder");
+
+        Statement st= con.createStatement();
+        ResultSet rs= st.executeQuery("SELECT * from tunes");
+
+        ArrayList<String> column = new ArrayList<String>();
+
+        while(rs.next())
+        {
+            column.add(rs.getString(Integer.parseInt(columnNr)));
+        }
+        return ok(showColumn.render(column));
+
     }
 }
